02571e90bee1ab76d4b1f53b7af9c04ec1c00bc3
Arman Jeevani <ajeevani1@myseneca.ca>
2025-09-03 02:16:37 -0400

    vite.config update

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,19 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tagger from "@dhiwise/component-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      // Map "components/XYZ" → "src/components/XYZ"
-      components: path.resolve(__dirname, 'src/components'),
-      pages:      path.resolve(__dirname, 'src/pages'),
-      hooks:      path.resolve(__dirname, 'src/hooks'),
-      services:   path.resolve(__dirname, 'src/services'),
-      contexts:   path.resolve(__dirname, 'src/contexts'),
-      utils:      path.resolve(__dirname, 'src/utils'),
-      styles:     path.resolve(__dirname, 'src/styles')
-    }
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
+  build: {
+    outDir: "build",
+    chunkSizeWarningLimit: 2000,
+  },
+  plugins: [tsconfigPaths(), react(), tagger()],
+  server: {
+    port: "4028",
+    host: "0.0.0.0",
+    strictPort: true,
+    allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
   }
 });
